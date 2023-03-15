8c6ba961c8fc04980a91b9b82ff4254668f3a413
hanzlasaadi <hanzalasaadi@gmail.com>
2023-03-16 01:46:53 +0500

    filling html templates with data

--- index.js
+++ index.js
@@ -32,21 +32,50 @@ const server = http.createServer((req, res) => {
 server.listen("8080", "127.0.0.1", () => {
   console.log("Started listening on port 8080");
 });
+*/
 
+const getTemplate = (obj, markup) => {
+  let output = markup.replace(/{%IMAGE%}/g, obj.image);
+  output = output.replace(/{%PRODUCTNAME%}/g, obj.productName);
+  output = output.replace(/{%ID%}/g, obj.id);
+  output = output.replace(/{%FROM%}/g, obj.from);
+  output = output.replace(/{%QUANTITY%}/g, obj.quantity);
+  output = output.replace(/{%NUTRIENTS%}/g, obj.nutrients);
+  output = output.replace(/{%PRICE%}/g, obj.price);
+  output = output.replace(/{%DESCRIPTION%}/g, obj.description);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
-const dataObj = JSON.parse(data);
-// console.log(data);
+  if (!obj.organic) output = output.replace(/{%ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf8"
+);
+const tempCards = fs.readFileSync(
+  `${__dirname}/templates/template-cards.html`,
+  "utf8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf8"
+);
 
 //Routes
 const server = http.createServer((req, res) => {
-  //   console.log(req.url);
   const reqPathName = req.url;
+  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf8");
+  const dataObj = JSON.parse(data);
 
   switch (reqPathName) {
     case "/":
     case "/home":
-      res.end("You are on home page.");
+      const cards = dataObj
+        .map((data) => getTemplate(data, tempCards))
+        .join("");
+      const output = tempOverview.replace("{%CARDS%}", cards);
+      res.end(output);
       break;
     case "/product":
       res.end("You are on the product page.");
@@ -68,4 +97,3 @@ const server = http.createServer((req, res) => {
 server.listen("8080", "127.0.0.1", () => {
   console.log("Started listening on port 8080");
 });
-*/
