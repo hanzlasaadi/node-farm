f561de629f81e032f940fab7ecb6639d97c706c8
hanzlasaadi <hanzalasaadi@gmail.com>
2023-07-01 09:45:25 +0500

    API

--- index.js
+++ index.js
@@ -34,13 +34,12 @@ const server = http.createServer((req,res) => {
 server.listen(8080, "127.0.0.1", () => {
   console.log("Listening on port 8080");
 });
-*/
 
 //----------------Routing-----------------
 const server = http.createServer((req,res) => {
   console.log(req.url)
   let pathname = req.url;
-
+  
   if(pathname === "" || pathname === "/" || pathname === "/overview" ) {
     res.end("<h1>HELLO, from the dark side of OVERVIEW</h1>");
   }
@@ -53,6 +52,43 @@ const server = http.createServer((req,res) => {
   }
 })
 
+server.listen(8080, "127.0.0.1", () => {
+  console.log("Listening on port 8080");
+});
+*/
+
+
+//----------------API-----------------
+// const data = fs.readFileSync("./dev-data/data.json");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`); // "__dirname" is the path of the where the index.js file is KEPT, not where it's RUNNING from
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req,res) => {
+  console.log(req.url)
+  let pathname = req.url;
+
+  if(pathname === "" || pathname === "/" || pathname === "/overview" ) {
+    res.writeHead(200, {
+      "Content-type": "text/html"
+    });
+    // res.end(`<img src="./dev-data/hello.png" height=500px>`);
+    // res.end(`./dev-data/hello.png`);
+    // res.end(`HELLOOO`);
+  }
+  else if(pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json" //browser knows what to expect
+    })
+    res.end(data);
+  }
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "header-type": "fucked header"
+    })
+    res.end("<h1>PAGE NOT FOUND</h1>");
+  }
+})
 server.listen(8080, "127.0.0.1", () => {
   console.log("Listening on port 8080");
 });
